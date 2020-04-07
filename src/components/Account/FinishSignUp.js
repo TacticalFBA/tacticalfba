86b9a5ecbb59f4b9fa7a3ccc877df5ac277a054d
kiki <daxiaojie11@gmail.com>
2020-04-07 10:47:19 +0800

    test sign in

--- src/components/Account/FinishSignUp.js
+++ src/components/Account/FinishSignUp.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import { UserConsumer } from "../../contexts/UserContext"
+import React from "react";
+import { UserConsumer } from "../../contexts/UserContext";
+import Button from "@material-ui/core/Button";
+import Backdrop from "@material-ui/core/Backdrop";
+import Box from "@material-ui/core/Box";
 
 export default function FinishSignUp() {
-    return (
-        <div>
-            <UserConsumer>
-                {({ SignIn }) => {
-                    return (
-                        <button onClick={() => SignIn()}>Sign In</button>
-                    )
-                }}
-            </UserConsumer>
-        </div>
-    )
+  return (
+    // <Box className="juzhong" style={{ height: "90vh" }}>
+    //   <Backdrop open={true} style={{ color: "#fff", zIndex: 1 }}></Backdrop>
+    <UserConsumer>
+      {({ SignIn }) => {
+        SignIn();
+        //   return (
+        //     <Button
+        //       variant="contained"
+        //       color="primary"
+        //       style={{ zIndex: 2 }}
+        //       onClick={() => SignIn()}
+        //     >
+        //       Sign Me In
+        //     </Button>
+        //   );
+      }}
+    </UserConsumer>
+    // </Box>
+  );
 }
